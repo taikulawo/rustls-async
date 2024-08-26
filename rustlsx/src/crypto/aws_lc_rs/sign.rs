3e313c260299c15497d11ee5e9ee0586d103d433
taikulawo <qaq1362211689@gmail.com>
2024-08-26 22:07:42 +0800

    add rustlx

---
+++ rustlsx/src/crypto/aws_lc_rs/sign.rs
@@ -0,0 +1,395 @@
+#![allow(clippy::duplicate_mod)]
+
+use alloc::boxed::Box;
+use alloc::string::ToString;
+use alloc::sync::Arc;
+use alloc::vec::Vec;
+use alloc::{format, vec};
+use async_trait::async_trait;
+use core::fmt::{self, Debug, Formatter};
+
+use pki_types::{PrivateKeyDer, PrivatePkcs8KeyDer, SubjectPublicKeyInfoDer};
+use webpki::alg_id;
+
+use super::ring_like::rand::SystemRandom;
+use super::ring_like::signature::{self, EcdsaKeyPair, Ed25519KeyPair, KeyPair, RsaKeyPair};
+use crate::crypto::signer::{public_key_to_spki, Signer, SigningKey};
+use crate::enums::{SignatureAlgorithm, SignatureScheme};
+use crate::error::Error;
+
+/// Parse `der` as any supported key encoding/type, returning
+/// the first which works.
+pub fn any_supported_type(der: &PrivateKeyDer<'_>) -> Result<Arc<dyn SigningKey>, Error> {
+    if let Ok(rsa) = RsaSigningKey::new(der) {
+        return Ok(Arc::new(rsa));
+    }
+
+    if let Ok(ecdsa) = any_ecdsa_type(der) {
+        return Ok(ecdsa);
+    }
+
+    if let PrivateKeyDer::Pkcs8(pkcs8) = der {
+        if let Ok(eddsa) = any_eddsa_type(pkcs8) {
+            return Ok(eddsa);
+        }
+    }
+
+    Err(Error::General(
+        "failed to parse private key as RSA, ECDSA, or EdDSA".into(),
+    ))
+}
+
+/// Parse `der` as any ECDSA key type, returning the first which works.
+///
+/// Both SEC1 (PEM section starting with 'BEGIN EC PRIVATE KEY') and PKCS8
+/// (PEM section starting with 'BEGIN PRIVATE KEY') encodings are supported.
+pub fn any_ecdsa_type(der: &PrivateKeyDer<'_>) -> Result<Arc<dyn SigningKey>, Error> {
+    if let Ok(ecdsa_p256) = EcdsaSigningKey::new(
+        der,
+        SignatureScheme::ECDSA_NISTP256_SHA256,
+        &signature::ECDSA_P256_SHA256_ASN1_SIGNING,
+    ) {
+        return Ok(Arc::new(ecdsa_p256));
+    }
+
+    if let Ok(ecdsa_p384) = EcdsaSigningKey::new(
+        der,
+        SignatureScheme::ECDSA_NISTP384_SHA384,
+        &signature::ECDSA_P384_SHA384_ASN1_SIGNING,
+    ) {
+        return Ok(Arc::new(ecdsa_p384));
+    }
+
+    if let Ok(ecdsa_p521) = EcdsaSigningKey::new(
+        der,
+        SignatureScheme::ECDSA_NISTP521_SHA512,
+        &signature::ECDSA_P521_SHA512_ASN1_SIGNING,
+    ) {
+        return Ok(Arc::new(ecdsa_p521));
+    }
+
+    Err(Error::General(
+        "failed to parse ECDSA private key as PKCS#8 or SEC1".into(),
+    ))
+}
+
+/// Parse `der` as any EdDSA key type, returning the first which works.
+pub fn any_eddsa_type(der: &PrivatePkcs8KeyDer<'_>) -> Result<Arc<dyn SigningKey>, Error> {
+    // TODO: Add support for Ed448
+    Ok(Arc::new(Ed25519SigningKey::new(
+        der,
+        SignatureScheme::ED25519,
+    )?))
+}
+
+/// A `SigningKey` for RSA-PKCS1 or RSA-PSS.
+///
+/// This is used by the test suite, so it must be `pub`, but it isn't part of
+/// the public, stable, API.
+#[doc(hidden)]
+pub struct RsaSigningKey {
+    key: Arc<RsaKeyPair>,
+}
+
+static ALL_RSA_SCHEMES: &[SignatureScheme] = &[
+    SignatureScheme::RSA_PSS_SHA512,
+    SignatureScheme::RSA_PSS_SHA384,
+    SignatureScheme::RSA_PSS_SHA256,
+    SignatureScheme::RSA_PKCS1_SHA512,
+    SignatureScheme::RSA_PKCS1_SHA384,
+    SignatureScheme::RSA_PKCS1_SHA256,
+];
+
+impl RsaSigningKey {
+    /// Make a new `RsaSigningKey` from a DER encoding, in either
+    /// PKCS#1 or PKCS#8 format.
+    pub fn new(der: &PrivateKeyDer<'_>) -> Result<Self, Error> {
+        let key_pair = match der {
+            PrivateKeyDer::Pkcs1(pkcs1) => RsaKeyPair::from_der(pkcs1.secret_pkcs1_der()),
+            PrivateKeyDer::Pkcs8(pkcs8) => RsaKeyPair::from_pkcs8(pkcs8.secret_pkcs8_der()),
+            _ => {
+                return Err(Error::General(
+                    "failed to parse RSA private key as either PKCS#1 or PKCS#8".into(),
+                ));
+            }
+        }
+        .map_err(|key_rejected| {
+            Error::General(format!("failed to parse RSA private key: {}", key_rejected))
+        })?;
+
+        Ok(Self {
+            key: Arc::new(key_pair),
+        })
+    }
+}
+
+impl SigningKey for RsaSigningKey {
+    fn choose_scheme(&self, offered: &[SignatureScheme]) -> Option<Box<dyn Signer>> {
+        ALL_RSA_SCHEMES
+            .iter()
+            .find(|scheme| offered.contains(scheme))
+            .map(|scheme| RsaSigner::new(Arc::clone(&self.key), *scheme))
+    }
+
+    fn public_key(&self) -> Option<SubjectPublicKeyInfoDer<'_>> {
+        Some(public_key_to_spki(
+            &alg_id::RSA_ENCRYPTION,
+            self.key.public_key(),
+        ))
+    }
+
+    fn algorithm(&self) -> SignatureAlgorithm {
+        SignatureAlgorithm::RSA
+    }
+}
+
+impl Debug for RsaSigningKey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("RsaSigningKey")
+            .field("algorithm", &self.algorithm())
+            .finish()
+    }
+}
+
+struct RsaSigner {
+    key: Arc<RsaKeyPair>,
+    scheme: SignatureScheme,
+    encoding: &'static dyn signature::RsaEncoding,
+}
+
+impl RsaSigner {
+    fn new(key: Arc<RsaKeyPair>, scheme: SignatureScheme) -> Box<dyn Signer> {
+        let encoding: &dyn signature::RsaEncoding = match scheme {
+            SignatureScheme::RSA_PKCS1_SHA256 => &signature::RSA_PKCS1_SHA256,
+            SignatureScheme::RSA_PKCS1_SHA384 => &signature::RSA_PKCS1_SHA384,
+            SignatureScheme::RSA_PKCS1_SHA512 => &signature::RSA_PKCS1_SHA512,
+            SignatureScheme::RSA_PSS_SHA256 => &signature::RSA_PSS_SHA256,
+            SignatureScheme::RSA_PSS_SHA384 => &signature::RSA_PSS_SHA384,
+            SignatureScheme::RSA_PSS_SHA512 => &signature::RSA_PSS_SHA512,
+            _ => unreachable!(),
+        };
+
+        Box::new(Self {
+            key,
+            scheme,
+            encoding,
+        })
+    }
+}
+
+#[async_trait]
+impl Signer for RsaSigner {
+    async fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
+        let mut sig = vec![0; self.key.public_modulus_len()];
+
+        let rng = SystemRandom::new();
+        self.key
+            .sign(self.encoding, &rng, message, &mut sig)
+            .map(|_| sig)
+            .map_err(|_| Error::General("signing failed".to_string()))
+    }
+
+    fn scheme(&self) -> SignatureScheme {
+        self.scheme
+    }
+}
+
+impl Debug for RsaSigner {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("RsaSigner")
+            .field("scheme", &self.scheme)
+            .finish()
+    }
+}
+
+/// A SigningKey that uses exactly one TLS-level SignatureScheme
+/// and one ring-level signature::SigningAlgorithm.
+///
+/// Compare this to RsaSigningKey, which for a particular key is
+/// willing to sign with several algorithms.  This is quite poor
+/// cryptography practice, but is necessary because a given RSA key
+/// is expected to work in TLS1.2 (PKCS#1 signatures) and TLS1.3
+/// (PSS signatures) -- nobody is willing to obtain certificates for
+/// different protocol versions.
+///
+/// Currently this is only implemented for ECDSA keys.
+struct EcdsaSigningKey {
+    key: Arc<EcdsaKeyPair>,
+    scheme: SignatureScheme,
+}
+
+impl EcdsaSigningKey {
+    /// Make a new `ECDSASigningKey` from a DER encoding in PKCS#8 or SEC1
+    /// format, expecting a key usable with precisely the given signature
+    /// scheme.
+    fn new(
+        der: &PrivateKeyDer<'_>,
+        scheme: SignatureScheme,
+        sigalg: &'static signature::EcdsaSigningAlgorithm,
+    ) -> Result<Self, ()> {
+        let key_pair = match der {
+            PrivateKeyDer::Sec1(sec1) => {
+                EcdsaKeyPair::from_private_key_der(sigalg, sec1.secret_sec1_der())
+                    .map_err(|_| ())?
+            }
+            PrivateKeyDer::Pkcs8(pkcs8) => {
+                EcdsaKeyPair::from_pkcs8(sigalg, pkcs8.secret_pkcs8_der()).map_err(|_| ())?
+            }
+            _ => return Err(()),
+        };
+
+        Ok(Self {
+            key: Arc::new(key_pair),
+            scheme,
+        })
+    }
+}
+
+impl SigningKey for EcdsaSigningKey {
+    fn choose_scheme(&self, offered: &[SignatureScheme]) -> Option<Box<dyn Signer>> {
+        if offered.contains(&self.scheme) {
+            Some(Box::new(EcdsaSigner {
+                key: Arc::clone(&self.key),
+                scheme: self.scheme,
+            }))
+        } else {
+            None
+        }
+    }
+
+    fn public_key(&self) -> Option<SubjectPublicKeyInfoDer<'_>> {
+        let id = match self.scheme {
+            SignatureScheme::ECDSA_NISTP256_SHA256 => alg_id::ECDSA_P256,
+            SignatureScheme::ECDSA_NISTP384_SHA384 => alg_id::ECDSA_P384,
+            SignatureScheme::ECDSA_NISTP521_SHA512 => alg_id::ECDSA_P521,
+            _ => unreachable!(),
+        };
+
+        Some(public_key_to_spki(&id, self.key.public_key()))
+    }
+
+    fn algorithm(&self) -> SignatureAlgorithm {
+        self.scheme.algorithm()
+    }
+}
+
+impl Debug for EcdsaSigningKey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("EcdsaSigningKey")
+            .field("algorithm", &self.algorithm())
+            .finish()
+    }
+}
+
+struct EcdsaSigner {
+    key: Arc<EcdsaKeyPair>,
+    scheme: SignatureScheme,
+}
+
+#[async_trait]
+impl Signer for EcdsaSigner {
+    async fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
+        let rng = SystemRandom::new();
+        self.key
+            .sign(&rng, message)
+            .map_err(|_| Error::General("signing failed".into()))
+            .map(|sig| sig.as_ref().into())
+    }
+
+    fn scheme(&self) -> SignatureScheme {
+        self.scheme
+    }
+}
+
+impl Debug for EcdsaSigner {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("EcdsaSigner")
+            .field("scheme", &self.scheme)
+            .finish()
+    }
+}
+
+/// A SigningKey that uses exactly one TLS-level SignatureScheme
+/// and one ring-level signature::SigningAlgorithm.
+///
+/// Compare this to RsaSigningKey, which for a particular key is
+/// willing to sign with several algorithms.  This is quite poor
+/// cryptography practice, but is necessary because a given RSA key
+/// is expected to work in TLS1.2 (PKCS#1 signatures) and TLS1.3
+/// (PSS signatures) -- nobody is willing to obtain certificates for
+/// different protocol versions.
+///
+/// Currently this is only implemented for Ed25519 keys.
+struct Ed25519SigningKey {
+    key: Arc<Ed25519KeyPair>,
+    scheme: SignatureScheme,
+}
+
+impl Ed25519SigningKey {
+    /// Make a new `Ed25519SigningKey` from a DER encoding in PKCS#8 format,
+    /// expecting a key usable with precisely the given signature scheme.
+    fn new(der: &PrivatePkcs8KeyDer<'_>, scheme: SignatureScheme) -> Result<Self, Error> {
+        match Ed25519KeyPair::from_pkcs8_maybe_unchecked(der.secret_pkcs8_der()) {
+            Ok(key_pair) => Ok(Self {
+                key: Arc::new(key_pair),
+                scheme,
+            }),
+            Err(e) => Err(Error::General(format!(
+                "failed to parse Ed25519 private key: {e}"
+            ))),
+        }
+    }
+}
+
+impl SigningKey for Ed25519SigningKey {
+    fn choose_scheme(&self, offered: &[SignatureScheme]) -> Option<Box<dyn Signer>> {
+        if offered.contains(&self.scheme) {
+            Some(Box::new(Ed25519Signer {
+                key: Arc::clone(&self.key),
+                scheme: self.scheme,
+            }))
+        } else {
+            None
+        }
+    }
+
+    fn public_key(&self) -> Option<SubjectPublicKeyInfoDer<'_>> {
+        Some(public_key_to_spki(&alg_id::ED25519, self.key.public_key()))
+    }
+
+    fn algorithm(&self) -> SignatureAlgorithm {
+        self.scheme.algorithm()
+    }
+}
+
+impl Debug for Ed25519SigningKey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Ed25519SigningKey")
+            .field("algorithm", &self.algorithm())
+            .finish()
+    }
+}
+
+struct Ed25519Signer {
+    key: Arc<Ed25519KeyPair>,
+    scheme: SignatureScheme,
+}
+
+#[async_trait]
+impl Signer for Ed25519Signer {
+    async fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
+        Ok(self.key.sign(message).as_ref().into())
+    }
+
+    fn scheme(&self) -> SignatureScheme {
+        self.scheme
+    }
+}
+
+impl Debug for Ed25519Signer {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Ed25519Signer")
+            .field("scheme", &self.scheme)
+            .finish()
+    }
+}
---
+++ rustlsx/src/crypto/ring/sign.rs
@@ -0,0 +1,425 @@
+#![allow(clippy::duplicate_mod)]
+
+use alloc::boxed::Box;
+use alloc::string::ToString;
+use alloc::sync::Arc;
+use alloc::vec::Vec;
+use alloc::{format, vec};
+use async_trait::async_trait;
+use core::fmt::{self, Debug, Formatter};
+
+use pki_types::{PrivateKeyDer, PrivatePkcs8KeyDer, SubjectPublicKeyInfoDer};
+use webpki::alg_id;
+
+use super::ring_like::rand::{SecureRandom, SystemRandom};
+use super::ring_like::signature::{self, EcdsaKeyPair, Ed25519KeyPair, KeyPair, RsaKeyPair};
+use crate::crypto::signer::{public_key_to_spki, Signer, SigningKey};
+use crate::enums::{SignatureAlgorithm, SignatureScheme};
+use crate::error::Error;
+use crate::x509::{wrap_concat_in_sequence, wrap_in_octet_string};
+
+/// Parse `der` as any supported key encoding/type, returning
+/// the first which works.
+pub fn any_supported_type(der: &PrivateKeyDer<'_>) -> Result<Arc<dyn SigningKey>, Error> {
+    if let Ok(rsa) = RsaSigningKey::new(der) {
+        return Ok(Arc::new(rsa));
+    }
+
+    if let Ok(ecdsa) = any_ecdsa_type(der) {
+        return Ok(ecdsa);
+    }
+
+    if let PrivateKeyDer::Pkcs8(pkcs8) = der {
+        if let Ok(eddsa) = any_eddsa_type(pkcs8) {
+            return Ok(eddsa);
+        }
+    }
+
+    Err(Error::General(
+        "failed to parse private key as RSA, ECDSA, or EdDSA".into(),
+    ))
+}
+
+/// Parse `der` as any ECDSA key type, returning the first which works.
+///
+/// Both SEC1 (PEM section starting with 'BEGIN EC PRIVATE KEY') and PKCS8
+/// (PEM section starting with 'BEGIN PRIVATE KEY') encodings are supported.
+pub fn any_ecdsa_type(der: &PrivateKeyDer<'_>) -> Result<Arc<dyn SigningKey>, Error> {
+    if let Ok(ecdsa_p256) = EcdsaSigningKey::new(
+        der,
+        SignatureScheme::ECDSA_NISTP256_SHA256,
+        &signature::ECDSA_P256_SHA256_ASN1_SIGNING,
+    ) {
+        return Ok(Arc::new(ecdsa_p256));
+    }
+
+    if let Ok(ecdsa_p384) = EcdsaSigningKey::new(
+        der,
+        SignatureScheme::ECDSA_NISTP384_SHA384,
+        &signature::ECDSA_P384_SHA384_ASN1_SIGNING,
+    ) {
+        return Ok(Arc::new(ecdsa_p384));
+    }
+
+    Err(Error::General(
+        "failed to parse ECDSA private key as PKCS#8 or SEC1".into(),
+    ))
+}
+
+/// Parse `der` as any EdDSA key type, returning the first which works.
+pub fn any_eddsa_type(der: &PrivatePkcs8KeyDer<'_>) -> Result<Arc<dyn SigningKey>, Error> {
+    // TODO: Add support for Ed448
+    Ok(Arc::new(Ed25519SigningKey::new(
+        der,
+        SignatureScheme::ED25519,
+    )?))
+}
+
+/// A `SigningKey` for RSA-PKCS1 or RSA-PSS.
+///
+/// This is used by the test suite, so it must be `pub`, but it isn't part of
+/// the public, stable, API.
+#[doc(hidden)]
+pub struct RsaSigningKey {
+    key: Arc<RsaKeyPair>,
+}
+
+static ALL_RSA_SCHEMES: &[SignatureScheme] = &[
+    SignatureScheme::RSA_PSS_SHA512,
+    SignatureScheme::RSA_PSS_SHA384,
+    SignatureScheme::RSA_PSS_SHA256,
+    SignatureScheme::RSA_PKCS1_SHA512,
+    SignatureScheme::RSA_PKCS1_SHA384,
+    SignatureScheme::RSA_PKCS1_SHA256,
+];
+
+impl RsaSigningKey {
+    /// Make a new `RsaSigningKey` from a DER encoding, in either
+    /// PKCS#1 or PKCS#8 format.
+    pub fn new(der: &PrivateKeyDer<'_>) -> Result<Self, Error> {
+        let key_pair = match der {
+            PrivateKeyDer::Pkcs1(pkcs1) => RsaKeyPair::from_der(pkcs1.secret_pkcs1_der()),
+            PrivateKeyDer::Pkcs8(pkcs8) => RsaKeyPair::from_pkcs8(pkcs8.secret_pkcs8_der()),
+            _ => {
+                return Err(Error::General(
+                    "failed to parse RSA private key as either PKCS#1 or PKCS#8".into(),
+                ));
+            }
+        }
+        .map_err(|key_rejected| {
+            Error::General(format!("failed to parse RSA private key: {}", key_rejected))
+        })?;
+
+        Ok(Self {
+            key: Arc::new(key_pair),
+        })
+    }
+}
+
+impl SigningKey for RsaSigningKey {
+    fn choose_scheme(&self, offered: &[SignatureScheme]) -> Option<Box<dyn Signer>> {
+        ALL_RSA_SCHEMES
+            .iter()
+            .find(|scheme| offered.contains(scheme))
+            .map(|scheme| RsaSigner::new(Arc::clone(&self.key), *scheme))
+    }
+
+    fn public_key(&self) -> Option<SubjectPublicKeyInfoDer<'_>> {
+        Some(public_key_to_spki(
+            &alg_id::RSA_ENCRYPTION,
+            self.key.public_key(),
+        ))
+    }
+
+    fn algorithm(&self) -> SignatureAlgorithm {
+        SignatureAlgorithm::RSA
+    }
+}
+
+impl Debug for RsaSigningKey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("RsaSigningKey")
+            .field("algorithm", &self.algorithm())
+            .finish()
+    }
+}
+
+struct RsaSigner {
+    key: Arc<RsaKeyPair>,
+    scheme: SignatureScheme,
+    encoding: &'static dyn signature::RsaEncoding,
+}
+
+impl RsaSigner {
+    fn new(key: Arc<RsaKeyPair>, scheme: SignatureScheme) -> Box<dyn Signer> {
+        let encoding: &dyn signature::RsaEncoding = match scheme {
+            SignatureScheme::RSA_PKCS1_SHA256 => &signature::RSA_PKCS1_SHA256,
+            SignatureScheme::RSA_PKCS1_SHA384 => &signature::RSA_PKCS1_SHA384,
+            SignatureScheme::RSA_PKCS1_SHA512 => &signature::RSA_PKCS1_SHA512,
+            SignatureScheme::RSA_PSS_SHA256 => &signature::RSA_PSS_SHA256,
+            SignatureScheme::RSA_PSS_SHA384 => &signature::RSA_PSS_SHA384,
+            SignatureScheme::RSA_PSS_SHA512 => &signature::RSA_PSS_SHA512,
+            _ => unreachable!(),
+        };
+
+        Box::new(Self {
+            key,
+            scheme,
+            encoding,
+        })
+    }
+}
+#[async_trait]
+impl Signer for RsaSigner {
+    async fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
+        let mut sig = vec![0; self.key.public().modulus_len()];
+
+        let rng = SystemRandom::new();
+        self.key
+            .sign(self.encoding, &rng, message, &mut sig)
+            .map(|_| sig)
+            .map_err(|_| Error::General("signing failed".to_string()))
+    }
+
+    fn scheme(&self) -> SignatureScheme {
+        self.scheme
+    }
+}
+
+impl Debug for RsaSigner {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("RsaSigner")
+            .field("scheme", &self.scheme)
+            .finish()
+    }
+}
+
+/// A SigningKey that uses exactly one TLS-level SignatureScheme
+/// and one ring-level signature::SigningAlgorithm.
+///
+/// Compare this to RsaSigningKey, which for a particular key is
+/// willing to sign with several algorithms.  This is quite poor
+/// cryptography practice, but is necessary because a given RSA key
+/// is expected to work in TLS1.2 (PKCS#1 signatures) and TLS1.3
+/// (PSS signatures) -- nobody is willing to obtain certificates for
+/// different protocol versions.
+///
+/// Currently this is only implemented for ECDSA keys.
+struct EcdsaSigningKey {
+    key: Arc<EcdsaKeyPair>,
+    scheme: SignatureScheme,
+}
+
+impl EcdsaSigningKey {
+    /// Make a new `ECDSASigningKey` from a DER encoding in PKCS#8 or SEC1
+    /// format, expecting a key usable with precisely the given signature
+    /// scheme.
+    fn new(
+        der: &PrivateKeyDer<'_>,
+        scheme: SignatureScheme,
+        sigalg: &'static signature::EcdsaSigningAlgorithm,
+    ) -> Result<Self, ()> {
+        let rng = SystemRandom::new();
+        let key_pair = match der {
+            PrivateKeyDer::Sec1(sec1) => {
+                Self::convert_sec1_to_pkcs8(scheme, sigalg, sec1.secret_sec1_der(), &rng)?
+            }
+            PrivateKeyDer::Pkcs8(pkcs8) => {
+                EcdsaKeyPair::from_pkcs8(sigalg, pkcs8.secret_pkcs8_der(), &rng).map_err(|_| ())?
+            }
+            _ => return Err(()),
+        };
+
+        Ok(Self {
+            key: Arc::new(key_pair),
+            scheme,
+        })
+    }
+
+    /// Convert a SEC1 encoding to PKCS8, and ask ring to parse it.  This
+    /// can be removed once <https://github.com/briansmith/ring/pull/1456>
+    /// (or equivalent) is landed.
+    fn convert_sec1_to_pkcs8(
+        scheme: SignatureScheme,
+        sigalg: &'static signature::EcdsaSigningAlgorithm,
+        maybe_sec1_der: &[u8],
+        rng: &dyn SecureRandom,
+    ) -> Result<EcdsaKeyPair, ()> {
+        let pkcs8_prefix = match scheme {
+            SignatureScheme::ECDSA_NISTP256_SHA256 => &PKCS8_PREFIX_ECDSA_NISTP256,
+            SignatureScheme::ECDSA_NISTP384_SHA384 => &PKCS8_PREFIX_ECDSA_NISTP384,
+            _ => unreachable!(), // all callers are in this file
+        };
+
+        let sec1_wrap = wrap_in_octet_string(maybe_sec1_der);
+        let pkcs8 = wrap_concat_in_sequence(pkcs8_prefix, &sec1_wrap);
+
+        EcdsaKeyPair::from_pkcs8(sigalg, &pkcs8, rng).map_err(|_| ())
+    }
+}
+
+// This is (line-by-line):
+// - INTEGER Version = 0
+// - SEQUENCE (privateKeyAlgorithm)
+//   - id-ecPublicKey OID
+//   - prime256v1 OID
+const PKCS8_PREFIX_ECDSA_NISTP256: &[u8] = b"\x02\x01\x00\
+      \x30\x13\
+      \x06\x07\x2a\x86\x48\xce\x3d\x02\x01\
+      \x06\x08\x2a\x86\x48\xce\x3d\x03\x01\x07";
+
+// This is (line-by-line):
+// - INTEGER Version = 0
+// - SEQUENCE (privateKeyAlgorithm)
+//   - id-ecPublicKey OID
+//   - secp384r1 OID
+const PKCS8_PREFIX_ECDSA_NISTP384: &[u8] = b"\x02\x01\x00\
+     \x30\x10\
+     \x06\x07\x2a\x86\x48\xce\x3d\x02\x01\
+     \x06\x05\x2b\x81\x04\x00\x22";
+
+impl SigningKey for EcdsaSigningKey {
+    fn choose_scheme(&self, offered: &[SignatureScheme]) -> Option<Box<dyn Signer>> {
+        if offered.contains(&self.scheme) {
+            Some(Box::new(EcdsaSigner {
+                key: Arc::clone(&self.key),
+                scheme: self.scheme,
+            }))
+        } else {
+            None
+        }
+    }
+
+    fn public_key(&self) -> Option<SubjectPublicKeyInfoDer<'_>> {
+        let id = match self.scheme {
+            SignatureScheme::ECDSA_NISTP256_SHA256 => alg_id::ECDSA_P256,
+            SignatureScheme::ECDSA_NISTP384_SHA384 => alg_id::ECDSA_P384,
+            _ => unreachable!(),
+        };
+
+        Some(public_key_to_spki(&id, self.key.public_key()))
+    }
+
+    fn algorithm(&self) -> SignatureAlgorithm {
+        self.scheme.algorithm()
+    }
+}
+
+impl Debug for EcdsaSigningKey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("EcdsaSigningKey")
+            .field("algorithm", &self.algorithm())
+            .finish()
+    }
+}
+
+struct EcdsaSigner {
+    key: Arc<EcdsaKeyPair>,
+    scheme: SignatureScheme,
+}
+#[async_trait]
+impl Signer for EcdsaSigner {
+    async fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
+        let rng = SystemRandom::new();
+        self.key
+            .sign(&rng, message)
+            .map_err(|_| Error::General("signing failed".into()))
+            .map(|sig| sig.as_ref().into())
+    }
+
+    fn scheme(&self) -> SignatureScheme {
+        self.scheme
+    }
+}
+
+impl Debug for EcdsaSigner {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("EcdsaSigner")
+            .field("scheme", &self.scheme)
+            .finish()
+    }
+}
+
+/// A SigningKey that uses exactly one TLS-level SignatureScheme
+/// and one ring-level signature::SigningAlgorithm.
+///
+/// Compare this to RsaSigningKey, which for a particular key is
+/// willing to sign with several algorithms.  This is quite poor
+/// cryptography practice, but is necessary because a given RSA key
+/// is expected to work in TLS1.2 (PKCS#1 signatures) and TLS1.3
+/// (PSS signatures) -- nobody is willing to obtain certificates for
+/// different protocol versions.
+///
+/// Currently this is only implemented for Ed25519 keys.
+struct Ed25519SigningKey {
+    key: Arc<Ed25519KeyPair>,
+    scheme: SignatureScheme,
+}
+
+impl Ed25519SigningKey {
+    /// Make a new `Ed25519SigningKey` from a DER encoding in PKCS#8 format,
+    /// expecting a key usable with precisely the given signature scheme.
+    fn new(der: &PrivatePkcs8KeyDer<'_>, scheme: SignatureScheme) -> Result<Self, Error> {
+        match Ed25519KeyPair::from_pkcs8_maybe_unchecked(der.secret_pkcs8_der()) {
+            Ok(key_pair) => Ok(Self {
+                key: Arc::new(key_pair),
+                scheme,
+            }),
+            Err(e) => Err(Error::General(format!(
+                "failed to parse Ed25519 private key: {e}"
+            ))),
+        }
+    }
+}
+
+impl SigningKey for Ed25519SigningKey {
+    fn choose_scheme(&self, offered: &[SignatureScheme]) -> Option<Box<dyn Signer>> {
+        if offered.contains(&self.scheme) {
+            Some(Box::new(Ed25519Signer {
+                key: Arc::clone(&self.key),
+                scheme: self.scheme,
+            }))
+        } else {
+            None
+        }
+    }
+
+    fn public_key(&self) -> Option<SubjectPublicKeyInfoDer<'_>> {
+        Some(public_key_to_spki(&alg_id::ED25519, self.key.public_key()))
+    }
+
+    fn algorithm(&self) -> SignatureAlgorithm {
+        self.scheme.algorithm()
+    }
+}
+
+impl Debug for Ed25519SigningKey {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Ed25519SigningKey")
+            .field("algorithm", &self.algorithm())
+            .finish()
+    }
+}
+
+struct Ed25519Signer {
+    key: Arc<Ed25519KeyPair>,
+    scheme: SignatureScheme,
+}
+#[async_trait]
+impl Signer for Ed25519Signer {
+    async fn sign(&self, message: &[u8]) -> Result<Vec<u8>, Error> {
+        Ok(self.key.sign(message).as_ref().into())
+    }
+
+    fn scheme(&self) -> SignatureScheme {
+        self.scheme
+    }
+}
+
+impl Debug for Ed25519Signer {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Ed25519Signer")
+            .field("scheme", &self.scheme)
+            .finish()
+    }
+}
